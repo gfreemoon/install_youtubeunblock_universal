203538b1b9e035d7f604ed406fa947d4a97799dc
gfreemoon <deniskazet@gmail.com>
2025-04-28 15:43:50 +0300

    Update install+config.sh

--- install+config.sh
+++ install+config.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-echo "1. Установка youtubeUnblock..."
-wget -qO- https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/install_youtubeUnblock.sh | sh
-
-echo "2. Генерация конфигурации..."
-wget -qO- https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/ytu_config_generator.sh | sh
-
-echo "3. Перезапуск youtubeUnblock..."
-if [ -f "/etc/init.d/youtubeUnblock" ]; then
-    /etc/init.d/youtubeUnblock restart
-else
-    echo "⚠️ Сервис youtubeUnblock не найден. Возможно, установка не удалась."
-fi
+curl -fsSL https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/install_youtubeUnblock.sh | sh
+curl -fsSL https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/ytu_config_generator.sh | sh
 
 echo "✅ Установка и настройка завершены!"
