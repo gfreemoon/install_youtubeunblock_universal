9d68337a19768494a08f513460849266f01913ff
gfreemoon <deniskazet@gmail.com>
2025-05-15 03:08:35 +0300

    Update install+config.sh

--- install+config.sh
+++ install+config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -fsSL https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/install_youtubeUnblock.sh | sh
-curl -fsSL https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/ytu_config_generator.sh | sh
+sh <(wget -O - https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/install_youtubeUnblock.sh)
+sh <(wget -O - https://raw.githubusercontent.com/gfreemoon/install_youtubeunblock_universal/refs/heads/main/ytu_config_generator.sh)
 
 echo "✅ Установка и настройка youtubeUnblock завершены!"
