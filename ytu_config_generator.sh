e3d5929c1b30cbdbc6e826fb25739c2fb9c63c08
gfreemoon <deniskazet@gmail.com>
2025-09-15 18:54:08 +0300

    Update ytu_config_generator.sh

--- ytu_config_generator.sh
+++ ytu_config_generator.sh
@@ -89,15 +89,6 @@ EOF
         "youtube-itdoginfo")
             echo "    list sni_domains 'play.google.com'" >> /etc/config/youtubeUnblock
             ;;
-        "Microsoft-Domains-HotCakeX")
-            echo "    list udp_dport_filter '88'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '3074'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '53'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '80'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '500'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '3544'" >> /etc/config/youtubeUnblock
-            echo "    list udp_dport_filter '4500'" >> /etc/config/youtubeUnblock
-            ;;
     esac
 
     rm -f /tmp/temp_list.txt
